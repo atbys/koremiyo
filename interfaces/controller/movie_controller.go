9320a6c947514f03254e2c75748d2a166c9b0225
atbys <ak.tamalin@gmail.com>
2021-03-14 21:21:38 +0900

    refactoring controller

--- interfaces/controller/movie_controller.go
+++ interfaces/controller/movie_controller.go
@@ -3,49 +3,120 @@ package controller
 import (
 	"net/http"
 
+	"github.com/atbys/koremiyo/domain"
 	"github.com/atbys/koremiyo/interfaces/cache"
-	"github.com/atbys/koremiyo/interfaces/presenter"
 	"github.com/atbys/koremiyo/interfaces/scraper"
-	"github.com/atbys/koremiyo/usecase"
+	"github.com/atbys/koremiyo/usecase/movie"
 )
 
 type MovieController struct {
-	Interactor usecase.MovieInteractor
+	Interactor movie.MovieInteractor
 }
 
 func NewMovieController(s scraper.Scraper, c cache.Cache) *MovieController {
 	mctrl := &MovieController{
-		Interactor: usecase.MovieInteractor{
-			MovieRepository: &scraper.MovieRepository{
+		Interactor: movie.MovieInteractor{
+			Repository: &scraper.MovieRepository{
 				Scraper: s,
 			},
-			MutualMovieCache: &cache.MovieCache{
+			MovieCache: &cache.MovieCache{
 				Cache: c,
 			},
-			MovieOutputPort: &presenter.HTTPPresenter{},
 		},
 	}
 
 	return mctrl
 }
 
-func (controller *MovieController) Index() (int, *usecase.OutputData) {
-	content, _ := controller.Interactor.GetRecommendation()
-	return http.StatusOK, content
+type Response struct {
+	Status  int
+	Message map[string]interface{}
 }
 
-func (controller *MovieController) Random() (int, *usecase.OutputData) {
-	content, _ := controller.Interactor.GetRandom()
-	// error handling
-	return http.StatusOK, content
+func (c *MovieController) Random() *Response {
+	res := NewResponse()
+
+	mov, err := c.Interactor.Random()
+	if err != nil {
+		res.Error(err)
+		return res
+	}
+
+	res.EmbedMovieInfo(mov)
+
+	return res
 }
 
-func (controller *MovieController) RandomClip(userId string) (int, *usecase.OutputData) {
-	content, _ := controller.Interactor.GetRandomFromClips(userId)
-	return http.StatusOK, content
+func (c *MovieController) RandomClip(uid string) *Response {
+	res := NewResponse()
+
+	m, err := c.Interactor.RandomFromClips(uid)
+	if err != nil {
+		res.Error(err)
+		return res
+	}
+
+	res.EmbedMovieInfo(m)
+
+	return res
 }
 
-func (controller *MovieController) MutualClip(userIds []string, cacheID int) (int, *usecase.OutputData) {
-	content, _ := controller.Interactor.GetMutualClip(userIds, cacheID)
-	return http.StatusOK, content
+func (c *MovieController) RandomMutual(uids []string, cacheID int) *Response {
+	res := NewResponse()
+
+	m, cacheID, err := c.Interactor.RandomFromMutual(uids, cacheID)
+	if err != nil {
+		res.Error(err)
+		return res
+	}
+
+	res.EmbedMovieInfo(m)
+	res.Message["cache_id"] = cacheID
+
+	return res
+}
+
+func (c *MovieController) MajorityMutual(uids []string, cacheID int) *Response {
+	res := NewResponse()
+
+	m, cacheID, err := c.Interactor.MajorityFromMutual(uids, cacheID)
+	if err != nil {
+		res.Error(err)
+		return res
+	}
+
+	res.EmbedMovieInfo(m)
+	res.Message["cache_id"] = cacheID
+
+	return res
+}
+
+func (c *MovieController) Index() *Response {
+	res := NewResponse()
+
+	res.Message["recommend"] = "GATTACA"
+
+	return res
+}
+
+func NewResponse() *Response {
+	res := &Response{
+		Status:  http.StatusOK,
+		Message: make(map[string]interface{}),
+	}
+
+	return res
+}
+
+func (res *Response) Error(err error) {
+	res.Status = http.StatusBadRequest
+	res.Message["error"] = err.Error()
+}
+
+func (res *Response) EmbedMovieInfo(m *domain.Movie) {
+	res.Message["movie_title"] = m.Title
+	res.Message["movie_link"] = m.FLink
+	res.Message["movie_rate"] = m.Rate //TODO: N.Nの形式に固定したいので文字列にする
+	res.Message["movie_abstruct"] = m.Abstruct
+	res.Message["movie_revies"] = m.Reviews
 }
